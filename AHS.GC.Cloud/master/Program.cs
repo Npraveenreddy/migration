57026740f67c36b1bc26e2156d9a4098d5a700af
PraveenReddy Narayana <narayana.praveen143@gmail.com>
2025-08-14 15:19:51 +0530

    Update Program.cs

--- AHS.GC.Cloud/master/Program.cs
+++ AHS.GC.Cloud/master/Program.cs
@@ -1,20 +1,28 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddControllers();
+
+// Swagger registration
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+});
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+// Enable Swagger in Development (or always)
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+});
 
 app.UseAuthorization();
-app.MapControllers();
 
-app.MapGet("/", () => "Hello from Portal API!");
+app.MapControllers();
 
 app.Run();
